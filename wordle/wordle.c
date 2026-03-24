009793167860372ec9e9ff9eef74e43422d9a414
Jerry Wang <jwang31@scu.edu>
2026-03-23 18:42:41 -0700

    added includes and main function skeleton

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -7,3 +7,13 @@ Author: Jerry Wang
 Description: driver program to solve a wordle puzzle
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(){
+    char guess[6];
+
+    return 0;
+}
