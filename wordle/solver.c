c89df2052257d6e9979168d599110475f76b7fd7
Jerry Wang <jwang31@scu.edu>
2026-03-24 19:12:06 -0700

    finished versions

--- wordle/solver.c
+++ wordle/solver.c
@@ -8,42 +8,50 @@ Description: functions for wordle.c
 
 */
 
-int DEBUG_S = 1;
+int DEBUG_S = 0;
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
-int is_valid_word(char *input);
+int load_allowed_word_list(char allowed_word_list[][6]);
+int is_valid_word(char *input, char allowed[][6], int size);
 int load_word_list(char word_list[][6]);
-void set_gray(char status_array[5][6]);
-void evaluate_guess(char guess[6], char target[6], char status_array[5][6]);
+void evaluate_guess(char guess[6], char target[6], char status_array[5][7]);
+int check_duplicate(char guess[6], char previous_guesses[][6], int count);
+int check_win(char status_array[5][7]);
 
-int is_valid_word(char *input){
+int load_allowed_word_list(char allowed_word_list[][6]){
     FILE *fp = fopen("allowed-words.txt", "r");
     assert(fp != NULL);
 
-    int flag = 0;
+    int index = 0;
 
-    //checking length validity
-    int length = strlen(input);
-    if (length != 5){
-        flag = 0;
-        fclose(fp);
-        return 0;
-    }
-
-    char buffer[6];
-    while(fscanf(fp, "%5s", buffer) == 1){
-        if (strcmp(buffer, input) == 0){
-            flag = 1;
-            return flag;
-        }
+    while(fscanf(fp, "%5s", allowed_word_list[index]) == 1){
+        index++;
     }
 
     fclose(fp);
-    return flag;
+
+    if(DEBUG_S == 1){
+        printf("First word from allowed-words.txt: %5s.\n", allowed_word_list[0]); // FIXED label
+        printf("Last word from allowed-words.txt: %5s.\n", allowed_word_list[index - 1]);
+    }
+
+    return index;
+}
+
+int is_valid_word(char *input, char allowed[][6], int size){
+    if (strlen(input) != 5) return 0;
+
+    for (int i = 0; i < size; i++){
+        if (strcmp(input, allowed[i]) == 0){
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 int load_word_list(char word_list[][6]){
@@ -66,14 +74,14 @@ int load_word_list(char word_list[][6]){
     return index;
 }
 
-void set_gray(char status_array[5][6]){
-    for (int i = 0; i < 5; i++) strcpy(status_array[i], "grey");
-}
+void evaluate_guess(char guess[6], char target[6], char status_array[5][7]){
+    int used[5] = {0, 0, 0, 0, 0};
 
-void evaluate_guess(char guess[6], char target[6], char status_array[5][6]){
-    int used[5] = {0, 0, 0, 0, 0}; //0 = unused letter
+    for (int i = 0; i < 5; i++){
+        strcpy(status_array[i], "grey");
+    }
 
-    //checking greens
+    // greens
     for (int i = 0; i < 5; i++){
         if (guess[i] == target[i]){
             strcpy(status_array[i], "green");
@@ -81,6 +89,7 @@ void evaluate_guess(char guess[6], char target[6], char status_array[5][6]){
         }
     }
 
+    // yellows
     for (int i = 0; i < 5; i++){
         if (strcmp(status_array[i], "green") == 0) continue;
 
@@ -90,14 +99,27 @@ void evaluate_guess(char guess[6], char target[6], char status_array[5][6]){
                 used[j] = 1;
                 break;
             }
-
         }
     }
+
+    if (DEBUG_S == 1) printf("DEBUG target: %s, guess: %s\n", target, guess);
+
+    for (int i = 0; i < 5; i++){
+        printf("[%s]", status_array[i]);
+    }
+    printf("\n");
 }
 
-/*
-function: 
+int check_duplicate(char guess[6], char previous_guesses[][6], int count){
+    for (int i = 0; i < count; i++){
+        if (strcmp(guess, previous_guesses[i]) == 0) return 0;
+    }
+    return 1;
+}
 
-description: 
-*/
-
+int check_win(char status_array[5][7]){
+    for (int i = 0; i < 5; i++){
+        if (strcmp(status_array[i], "green") != 0) return 0;
+    }
+    return 1;
+}
--- wordle/wordle.c
+++ wordle/wordle.c
@@ -8,7 +8,7 @@ Description: driver program to solve a wordle puzzle
 
 */
 
-int DEBUG_W = 1;
+int DEBUG_W = 0; //1 = debugging mode
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,10 +16,12 @@ int DEBUG_W = 1;
 #include <time.h>
 #include <assert.h>
 
-int is_valid_word(char *input);
+int load_allowed_word_list(char allowed_word_list[][6]);
+int is_valid_word(char *input, char allowed[][6], int size);
 int load_word_list(char word_list[][6]);
-void set_gray(char status_array[5][6]);
-void evaluate_guess(char guess[6], char target[6], char status_array[5][6]);
+void evaluate_guess(char guess[6], char target[6], char status_array[5][7]);
+int check_duplicate(char guess[6], char previous_guesses[][6], int count);
+int check_win(char status_array[5][7]);
 
 int main(){
     srand(time(NULL));
@@ -29,6 +31,9 @@ int main(){
     char guess[6];
     char target[6];
 
+    char allowed_words_array[16000][6];
+    int allowed_size = load_allowed_word_list(allowed_words_array); // FIXED name
+
     char word_array[11000][6];
     int word_array_size = load_word_list(word_array);
 
@@ -37,11 +42,11 @@ int main(){
 
     if(DEBUG_W == 1) printf("The selected answer from potential-answers.txt is: %5s.\n", target);
     
-
-
     //GAME START
     int win_flag = 0;
     int guess_tracker = 1;
+    char previous_guesses[6][6] = {0}; // safer initialization
+
     while (guess_tracker < 7){
         printf("\n");
         printf("You are on guess #%d.\n", guess_tracker);
@@ -49,35 +54,48 @@ int main(){
 
         scanf("%5s", guess);
 
-        if (is_valid_word(guess) != 1){
+        if (is_valid_word(guess, allowed_words_array, allowed_size) != 1){
             printf("Invalid word. Try again.\n");
             continue;
         }
 
-        //ACTUAL GAME LOGIC
+        if (check_duplicate(guess, previous_guesses, guess_tracker - 1) == 0){
+            printf("Already guessed. Try again.\n");
+            continue;
+        }
 
-        char status_array[5][6];
-        set_gray(status_array);
+        strcpy(previous_guesses[guess_tracker - 1], guess);
+
+        //ACTUAL GAME LOGIC
+        char status_array[5][7];
         evaluate_guess(guess, target, status_array);
             
+        if (guess_tracker != 1){
+            printf("Previous guesses: \n");
+            for (int i = 0; i < guess_tracker - 1; i++) printf("%5s\n", previous_guesses[i]);
+        }
+
         for (int i = 0; i < 5; i++) printf("    %c    ", guess[i]);
         printf("\n");
-        for (int i = 0; i < 5; i++) printf(" %s   ", status_array[i]);
+
+        for (int i = 0; i < 5; i++) printf("%-9s", status_array[i]);
         printf("\n");
 
-
-
-
-
+        if (check_win(status_array) == 1){
+            win_flag = 1;
+            break;
+        }
 
         guess_tracker++;
     }
 
     if (win_flag == 0){
         printf("You lost. Word was: %5s.\n", target);
+        printf("\n");
     }
     else{
-        printf("You won. Number of guesses: %s.\n", guess);
+        printf("You won. Number of guesses: %d.\n", guess_tracker); 
+        printf("\n");
     }
 
     return 0;
